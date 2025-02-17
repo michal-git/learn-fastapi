fd24fa491a0b41b499bf3e80e6cfe5210a5d69b2
michal-git <mumu.cisek@gmail.com>
2025-02-17 20:02:16 +0100

    Add relation between User and Exercise models

--- backend/app/models/exercise.py
+++ backend/app/models/exercise.py
@@ -4,8 +4,9 @@ from uuid import UUID, uuid4
 from sqlmodel import SQLModel, Field, Column, String, DateTime, Relationship
 from sqlalchemy import func
 
-from backend.app.models.fill_gap_sentence import FillGapSentence
-from backend.app.models.multiple_choice_question import MultipleChoiceQuestion
+from app.models.fill_gap_sentence import FillGapSentence
+from app.models.multiple_choice_question import MultipleChoiceQuestion
+from app.models.user import User
 
 
 class ExerciseType(SQLModel, table=True):
@@ -15,7 +16,7 @@ class ExerciseType(SQLModel, table=True):
 
 class Exercise(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
-    teacher_id: UUID = Field(foreign_key="teacher.id", nullable=False)
+    owner_id: UUID = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
     exercise_type_id: UUID = Field(foreign_key="exercisetype.id", nullable=False)
     title: str = Field(nullable=False)
     description: Optional[str] = Field(default=None, nullable=True)
@@ -33,6 +34,7 @@ class Exercise(SQLModel, table=True):
     )
 
     # Relationships
+    owner: Optional["User"] = Relationship(back_populates="exercises")
     exercise_type: ExerciseType = Relationship(back_populates="exercises")
     fill_gap_sentences: List["FillGapSentence"] = Relationship(
         back_populates="exercise"
